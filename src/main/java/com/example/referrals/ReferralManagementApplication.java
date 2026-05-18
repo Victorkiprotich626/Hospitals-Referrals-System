b2c995bce5a86fe0dc5f26378ca30f36dedde3cb
victor <kisingarr55@gmail.com>
2026-05-18 10:55:07 +0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/example/referrals/ReferralManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.referrals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReferralManagementApplication.class, args);
+    }
+}
