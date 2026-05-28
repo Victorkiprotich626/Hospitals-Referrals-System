37d24e2dc28b78adf8bea6714da6fff2bf8a2dbe
victor <kisingarr55@gmail.com>
2026-05-28 19:36:00 +0300

    Finish Patient

--- src/main/java/com/example/referrals/patient/Patient.java
+++ src/main/java/com/example/referrals/patient/Patient.java
@@ -62,3 +62,52 @@ public class Patient extends AuditableEntity {
     public String getFirstName() {
         return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
