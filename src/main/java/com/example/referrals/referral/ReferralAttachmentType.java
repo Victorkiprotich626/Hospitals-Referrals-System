e344d83d77ad7ccfc435851126ea35d632b4d82d
victor <kisingarr55@gmail.com>
2026-05-28 16:53:29 +0300

    Sketch out Referral Attachment Type structure

---
+++ src/main/java/com/example/referrals/referral/ReferralAttachmentType.java
@@ -0,0 +1,10 @@
+package com.example.referrals.referral;
+
+public enum ReferralAttachmentType {
+    REFERRAL_LETTER("Referral Letter"),
+    LAB_RESULT("Lab Result"),
+    IMAGING("Imaging"),
+    PRESCRIPTION("Prescription"),
+    DISCHARGE_SUMMARY("Discharge Summary"),
+    CLINICAL_NOTE("Clinical Note"),
+    OTHER("Other");
