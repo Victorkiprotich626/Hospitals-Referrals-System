55aaf3374062ad4d06a030aad1cdd05976af54dc
victor <kisingarr55@gmail.com>
2026-06-01 15:43:05 +0300

    Scaffold Custom User Details Service

---
+++ src/main/java/com/example/referrals/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.example.referrals.security;
+
+import com.example.referrals.user.AppUser;
+import com.example.referrals.user.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
