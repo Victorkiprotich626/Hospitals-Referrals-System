0f57ab2298cc436ddf0952e42acdde5ff0ae43e8
victor <kisingarr55@gmail.com>
2026-06-04 12:07:24 +0300

    Add Referral Assignment Form for hospital admin input validation

---
+++ src/main/java/com/example/referrals/hospitaladmin/form/ReferralAssignmentForm.java
@@ -0,0 +1,24 @@
+package com.example.referrals.hospitaladmin.form;
+
+public class ReferralAssignmentForm {
+
+    private Long departmentId;
+
+    private Long doctorId;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+}
