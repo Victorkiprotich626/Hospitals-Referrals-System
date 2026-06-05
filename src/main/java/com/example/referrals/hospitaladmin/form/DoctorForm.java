fe0ba6e608ea2b178123c76362cb8672cc778f50
victor <kisingarr55@gmail.com>
2026-06-05 16:32:44 +0300

    Flesh out Doctor Form

--- src/main/java/com/example/referrals/hospitaladmin/form/DoctorForm.java
+++ src/main/java/com/example/referrals/hospitaladmin/form/DoctorForm.java
@@ -48,3 +48,40 @@ public class DoctorForm {
     public String getEmail() {
         return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
