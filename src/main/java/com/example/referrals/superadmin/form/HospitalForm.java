3f6a4602e045371e0cafbc2c773bc4907bbc5067
victor <kisingarr55@gmail.com>
2026-06-15 10:51:25 +0300

    Round out Hospital Form

--- src/main/java/com/example/referrals/superadmin/form/HospitalForm.java
+++ src/main/java/com/example/referrals/superadmin/form/HospitalForm.java
@@ -41,3 +41,36 @@ public class HospitalForm {
     public void setCode(String code) {
         this.code = code;
     }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
