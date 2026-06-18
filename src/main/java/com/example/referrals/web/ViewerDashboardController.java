4d13ddae1280a02fa78f41904a0b628dadbd2634
victor <kisingarr55@gmail.com>
2026-06-18 19:30:35 +0300

    Sketch out Viewer Dashboard Controller structure

---
+++ src/main/java/com/example/referrals/web/ViewerDashboardController.java
@@ -0,0 +1,11 @@
+package com.example.referrals.web;
+
+import com.example.referrals.reporting.TenantReportingService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/viewer")
+public class ViewerDashboardController {
